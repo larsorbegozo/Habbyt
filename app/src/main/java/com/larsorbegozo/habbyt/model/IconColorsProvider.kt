c1f35c360743eb2c7f06b4423d067ff668c2ef97
LarsOrbegozo <wezenkerrash44@gmail.com>
2023-04-17 15:08:56 -0300

    Colors added

--- app/src/main/java/com/larsorbegozo/habbyt/model/IconColorsProvider.kt
+++ app/src/main/java/com/larsorbegozo/habbyt/model/IconColorsProvider.kt
@@ -5,9 +5,24 @@ import com.larsorbegozo.habbyt.R
 class IconColorsProvider {
     companion object {
         val habitIconColorLists: List<HabitIconColor> = listOf(
-            HabitIconColor(0, "orange", R.color.orange_200),
-            HabitIconColor(1, "red", R.color.red_700),
-            HabitIconColor(2, "red300", R.color.red_300)
+            HabitIconColor(0, "red", R.color.icon_red),
+            HabitIconColor(1, "pink", R.color.icon_pink),
+            HabitIconColor(2, "purple", R.color.icon_purple),
+            HabitIconColor(3, "deep_purple", R.color.icon_deep_purple),
+            HabitIconColor(4, "indigo", R.color.icon_indigo),
+            HabitIconColor(5, "blue", R.color.icon_blue),
+            HabitIconColor(6, "light_blue", R.color.icon_light_blue),
+            HabitIconColor(7, "cyan", R.color.icon_cyan),
+            HabitIconColor(8, "teal", R.color.icon_teal),
+            HabitIconColor(9, "green", R.color.icon_green),
+            HabitIconColor(10, "light_green", R.color.icon_light_green),
+            HabitIconColor(11, "lime", R.color.icon_lime),
+            HabitIconColor(12, "yellow", R.color.icon_yellow),
+            HabitIconColor(13, "amber", R.color.icon_amber),
+            HabitIconColor(14, "orange", R.color.icon_orange),
+            HabitIconColor(15, "deep_orange", R.color.icon_deep_orange),
+            HabitIconColor(16, "brown", R.color.icon_brown),
+            HabitIconColor(17, "gray", R.color.icon_gray),
         )
     }
 }
